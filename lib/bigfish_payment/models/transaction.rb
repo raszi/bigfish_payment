4138bb6d9d9cfb042159ed757f39b483408ef7b5
KARASZI István <github@spam.raszi.hu>
2011-10-05 20:35:33 +0200

    seems to be working

--- lib/bigfish_payment/models/transaction.rb
+++ lib/bigfish_payment/models/transaction.rb
@@ -1,86 +1,91 @@
-  module BigfishPayment
-    class Transaction < ::ActiveRecord::Base
+module BigfishPayment
 
-      has_many :logs, :class_name => 'BigfishPayment::TransactionLog', :dependent => :destroy, :foreign_key => 'bf_payment_transaction_id'
-      belongs_to :currency, :class_name => 'BigfishPayment::Currency', :foreign_key => 'bf_payment_currency_id'
-      belongs_to :provider, :class_name => 'BigfishPayment::Provider', :foreign_key => 'bf_payment_provider_id'
+  class Status
+    PENDING    = /^PEND/
+    SUCCESSFUL = /^SUCC/
+    ERROR      = /^ERR/
+    CANCELLED  = /^CANC/
+  end
 
-      validates_presence_of :response_url
-      validates_numericality_of :amount, :greater_than => 0
+  class Transaction < ::ActiveRecord::Base
 
-      # Initializes the transaction in the PHP way
-      #
-      # PHP equivalent:
-      # <tt>$paymentGateway->init(’CIB’, ’http://www.webaruhaz.hu/payment_gateway_valasz_url.php’, ’1500’, ’12345’, ’123’, ’HUF’, ’HU’);</tt>
-      def self.init(provider, response_url, amount, user_id=nil, currency='HUF', language='HU')
-        t = new(:amount => amount, :response_url => response_url, :currency => Currency.get(currency), :provider => Provider.get(provider))
+    has_many :logs, :class_name => 'BigfishPayment::TransactionLog', :dependent => :destroy, :foreign_key => 'bf_payment_transaction_id'
+    belongs_to :currency, :class_name => 'BigfishPayment::Currency', :foreign_key => 'bf_payment_currency_id'
+    belongs_to :provider, :class_name => 'BigfishPayment::Provider', :foreign_key => 'bf_payment_provider_id'
 
-        t.user_id = user_id if t.respond_to?(:user_id)
+    #validates_presence_of :provider
+    validates_presence_of :response_url
 
-        raise RuntimeError, "Could not create Transaction: #{t.errors.full_messages}" unless t.save
+    # Initializes the transaction
+    def init
+      raise RuntimeError, "Could not initialize transaction without amount" unless self.respond_to?(:amount)
 
-        t.init
+      raise RuntimeError, "Could not initialize transaction without response_url" unless self.response_url
+      raise RuntimeError, "Could not initialize transaction, already initialized" if self.transaction_id?
 
-        t
+      params = {
+        :ProviderName => self.provider.code,
+        :StoreName    => Config.get_store_name,
+        :Amount       => self.amount,
+        :Currency     => self.currency.code,
+        :ResponseUrl  => self.response_url
+      }
+
+      # if somebody extended us, we should send this as well
+      params[:UserId] = self.user_id if self.respond_to?(:user_id)
+      params[:OrderId] = self.order_id if self.respond_to?(:order_id)
+
+      response = Client.init(params)
+
+      t_id = response.optional['TransactionId']
+      if t_id
+        self.transaction_id = t_id
       end
 
-      # Initializes the transaction
-      def init
-        raise RuntimeError, "Could not initialize transaction, already initialized" if transaction_id?
+      log = TransactionLog.from_response(response)
+      self.logs << log
+      raise RuntimeError, "Could not update the Transaction: #{self.errors.full_messages}" unless self.save
 
-        params = {
-          :ProviderName => self.provider.code,
-          :StoreName    => Config.get_store_name,
-          :Amount       => amount,
-          :OrderId      => self.id,
-          :Currency     => self.currency.code,
-          :ResponseUrl  => self.response_url
-        }
+      log.code.match(Status::SUCCESSFUL)
+    end
 
-        # if somebody extended us, we should send this as well
-        params[:UserId] = self.user_id if self.respond_to?(:user_id)
+    # Starts the transaction
+    def start
+      raise RuntimeError, "Could not start an uninitialized transaction" unless transaction_id?
 
-        response = Client.init(params)
+      params = {
+        :TransactionId => self.transaction_id
+      }
 
-        t_id = response.optional['TransactionId']
-        if t_id
-          self.transaction_id = t_id
-        end
+      Client.start(params)
+    end
 
-        self.logs << TransactionLog.from_response(response)
-        raise RuntimeError, "Could not update the Transaction: #{self.errors.full_messages}" unless self.save
+    def result
+      params = {
+        :TransactionId => self.transaction_id
+      }
+
+      response = Client.result(params)
+
+      # validating
+      optional = response.optional
+      raise RuntimeError, "Invalid response: TransactionId: #{optional['TransactionId']}" unless optional['TransactionId'] == self.transaction_id
+
+      # double check if possible
+      if self.respond_to?(:order_id)
+        raise RuntimeError, "Invalid response: OrderId: #{optional['OrderId']}" unless optional['OrderId'] == self.order_id.to_s
       end
 
-      # Starts the transaction
-      def start
-        raise RuntimeError, "Could not start an uninitialized transaction" unless transaction_id?
-
-        params = {
-          :TransactionId => self.transaction_id
-        }
-
-        Client.start(params)
+      if self.respond_to?(:user_id)
+        raise RuntimeError, "Invalid response: UserId: #{optional['UserId']}" unless optional['UserId'] == self.user_id.to_s
       end
 
-      def result
-        params = {
-          :TransactionId => self.transaction_id
-        }
+      log = TransactionLog.from_response(response)
+      self.logs << log
+      raise RuntimeError, "Could not update the Transaction: #{self.errors.full_messages}" unless self.save
 
-        response = Client.result(params)
-
-        # validating
-        optional = response.optional
-        raise RuntimeError, "Invalid response: TransactionId: #{optional['TransactionId']}" unless optional['TransactionId'] == self.transaction_id
-        raise RuntimeError, "Invalid response: OrderId: #{optional['OrderId']}" unless optional['OrderId'] == self.id.to_s
-
-        if self.respond_to?(:user_id)
-          raise RuntimeError, "Invalid response: UserId: #{optional['UserId']}" unless optional['UserId'] == self.user_id.to_s
-        end
-
-        self.logs << TransactionLog.from_response(response)
-        raise RuntimeError, "Could not update the Transaction: #{self.errors.full_messages}" unless self.save
-      end
+      log.code
+    end
 
   end
 end
